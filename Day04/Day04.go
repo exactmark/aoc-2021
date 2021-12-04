daa21b266b7f10aaa1acf2ad66f0d736548491f2
Mark F <exactmark@gmail.com>
2021-12-04 08:58:20 -0500

    Day04 Start
    It's so empty

---
+++ Day04/Day04.go
@@ -0,0 +1,16 @@
+package Day04
+
+
+func solvePt1(inputLines []string) {
+
+
+}
+
+func solvePt2(inputLines []string) {
+
+}
+
+func Solve(inputLines []string) {
+	solvePt1(inputLines)
+	//solvePt2(inputLines)
+}
