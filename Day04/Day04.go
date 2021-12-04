db1bf3e4a6155cf778c1d596b45300abb9c94f37
Mark F <exactmark@gmail.com>
2021-12-04 09:42:16 -0500

    Day 04
    Started struct generation

--- Day04/Day04.go
+++ Day04/Day04.go
@@ -1,8 +1,48 @@
 package Day04
 
+import (
+	"strconv"
+	"strings"
+)
+
+type empty struct {
+}
+
+type coord struct {
+	x int
+	y int
+}
+
+type bingoBoard struct {
+	heldNumbers    map[int]coord
+	occupiedSpaces map[coord]empty
+}
+
+func generateBoard(inputLines []string)*bingoBoard{
+	newBoard:=bingoBoard{
+		heldNumbers:    nil,
+		occupiedSpaces: nil,
+	}
+
+	return &newBoard
+}
 
 func solvePt1(inputLines []string) {
 
+	var randomNumbers []int
+
+	lineSplit := strings.Split(inputLines[0], ",")
+	for _, v := range lineSplit {
+		singleNum, _ := strconv.Atoi(v)
+		randomNumbers = append(randomNumbers, singleNum)
+	}
+
+	var allBoards []*bingoBoard
+
+	for i := 1; i < len(inputLines); i += 6 {
+		nextBoard := generateBoard(inputLines[i+1 : i+6])
+		allBoards= append(allBoards,nextBoard)
+	}
 
 }
 
